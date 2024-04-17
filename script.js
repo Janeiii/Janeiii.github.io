b4a17ed3c02c521ebb77c81202ae600e9d634084
Janet Lin <linxiaoyingw@gmail.com>
2024-04-16 17:27:37 -0700

    Update

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-<script>
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
-
-                document.querySelector(this.getAttribute('href')).scrollIntoView({
-                    behavior: 'smooth'
-                });
-            });
-        });
-
-        // Add click event listeners to bubbles
-        document.querySelectorAll('.bubble').forEach(bubble => {
-        bubble.addEventListener('click', () => {
-            // Redirect to project page when bubble is clicked
-            window.location.href = bubble.querySelector('a').getAttribute('href');
-        });
-        });
-
-</script>
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
